13b2ff8e342bc44ed928fc808ab9a4d594e095d1
rohan mahajan <rohanm@dhcp-18-111-99-137.dyn.mit.edu>
2015-03-09 01:21:01 -0400

    got basic processing working

---
+++ tree_node.py
@@ -0,0 +1,31 @@
+import random
+import simpy
+
+
+VECTOR_SIZE = 100 ## number of elements in vector
+VALUE_RANGE = 100.0 ## highest value range for element in vector
+LATENCY_RANGE = 100.0 ## range from zero for bottleneck link
+PROCESS_RANGE = 1.0 ## the process range
+class Tree(object):
+     def __init__(self, env):
+         self.env = env
+	 self.vector = []
+	 for i in range(VECTOR_SIZE):
+	     random_value = random.random() * VALUE_RANGE
+	     self.vector.append(random_value)
+         # Start the run process everytime an instance is created.
+         self.action = env.process(self.run())
+     
+     def process_data(self):
+	 process_time = random.random() * PROCESS_RANGE
+	 yield self.env.timeout(process_time)	 
+     
+     def run(self):
+         while True:
+	     yield self.env.process(self.process_data())
+             print('Processingat %d' % self.env.now)
+
+
+env = simpy.Environment()
+tree = Tree(env)
+env.run(until=15)
