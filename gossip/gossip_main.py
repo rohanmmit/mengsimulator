d6323103c4d828247d68a7a1eaab302c457c8ca4
rohan mahajan <rohanm@mit.edu>
2015-03-16 22:43:36 -0400

    finished the gossip protcol

--- gossip/gossip_main.py
+++ gossip/gossip_main.py
@@ -3,12 +3,12 @@ import random
 import matplotlib.pyplot as plt
 from heapq import heappush, heappop
 from node import *
-
+from match import *
 error_cutoff = 0.05
 lambd = 1
 
 def is_close(value, mean):
-  diff = math.abs(value - mean)
+  diff = abs(value - mean)
   if diff < error_cutoff * mean:
       return True
   return False
@@ -32,10 +32,49 @@ def setup(num_nodes):
     node = Node(value,i, None)
     available_nodes.append(node)
     mean = (mean * i + value)/(i + 1)
-  return available_nodes,  
+  return available_nodes, node_dict, mean 
 
+def process_match(match, node_dict, available_nodes):
+   node1, node2 = match.get_nodes()
+   node1_id, _, node1_value = node1.get_info()
+   node2_id, _, node2_value = node2.get_info()
+   average = (node1_value + node2_value) / 2
+   node_dict[node1_id] = average
+   node_dict[node2_id] = average
+   node1 = Node(average, node1_id, node2_id)
+   node2 = Node(average, node2_id, node1_id)
+   available_nodes.append(node1)  
+   available_nodes.append(node2)  
+
+def match_nodes(available_nodes, event_processing, current_time):
+    i = 0
+    while i < len(available_nodes):
+       current_node = available_nodes[i]
+       _,past_id, _ = current_node.get_info()
+       for j in range(i+1, len(available_nodes)):
+          temp_node = available_nodes[j]
+          temp_node_id, _,_ = temp_node.get_info()
+          if temp_node_id != past_id:
+             available_nodes.pop(j)
+             available_nodes.pop(i)
+             random_delay = get_delay() + current_time
+             new_match = Match(current_node, temp_node)
+	     heappush(event_processing, (random_delay,new_match))        
+             i = i -1
+             break 
+       i = i + 1     
 def run_test(num_nodes):
-    return num_nodes
+    available_nodes, node_dict, mean = setup(num_nodes)
+    current_time = 0.0
+    event_processing = []
+    while not isFinished(node_dict, mean):
+       print node_dict 
+       match_nodes(available_nodes, event_processing, current_time)
+       new_time, match = heappop(event_processing)     
+       current_time = new_time
+       process_match(match, node_dict, available_nodes)
+    print current_time
+    return current_time
 
 def run_multiple_tests(num_nodes):
     average_time = 0.0
@@ -51,9 +90,5 @@ def run():
       average_time = run_multiple_tests(num_nodes) 
       times_list.append(average_time) 
    plt.plot(num_nodes_list, times_list)
-event_processing = []
-heappush(event_processing, (5.2,"write code"))        
-heappush(event_processing, (5.3,"fuck"))
-result = heappop(event_processing)        
-run()
+result = run_test(10)
 
--- gossip/node.py
+++ gossip/node.py
@@ -4,8 +4,8 @@ class Node(object):
       self.my_id = my_id
       self.last_matched_id = last_matched_id
   
-  def get_id(self):
-      return self.my_id
+  def get_info(self):
+      return self.my_id, self.last_matched_id, self.value
 
-  def get_last_matched_id(self):
-      return self.last_matched_id
+  def __repr__(self):
+        return "<my id %s target id:%s>" % (self.my_id, self.last_matched_id)
