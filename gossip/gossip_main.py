24466c71d86c3f50772b4eaac08c92831ed90f14
rohan mahajan <rohanm@rohans-MacBook-Pro-2.local>
2015-04-07 14:04:37 -0400

    more stuff

--- gossip/gossip_main.py
+++ gossip/gossip_main.py
@@ -8,6 +8,7 @@ from match import *
 error_cutoff = 0.05
 lambd = 1
 
+@profile
 def is_close(vector, mean_vector):
   mean_sum = numpy.linalg.norm(mean_vector, 2) 
   diff = numpy.linalg.norm(vector-mean_vector, 2)
@@ -15,18 +16,31 @@ def is_close(vector, mean_vector):
       return True
   return False
 
+@profile
 def get_pareto_delay():
    s = numpy.random.pareto(5, 2)
    return (s[0] + s[1]) 
 
+@profile
 def get_exponential_delay():
   return random.expovariate(lambd) * 2
 
+def create_matrix(vector_size):
+  num_rows = vector_size
+  num_cols = vector_size/10  
+  train = numpy.zeros([num_rows, num_cols], float)
+  for col in range(num_cols):
+   train[col*10:(col +1) * 10,col] = 0.1
+  return train
+
+
+@profile
 def setup(num_nodes):
   available_nodes = []
   unfinished_nodes = dict()
-  vector_size = 1000
+  vector_size = 10
   mean_vector = [0.0] * vector_size
+  matrix_wrapper = create_matrix(vector_size)
   for i in range(num_nodes):
     node_vector = []
     unfinished_nodes[i] = 0
@@ -35,18 +49,19 @@ def setup(num_nodes):
       node_vector.append(value)
       mean_vector[j] = (mean_vector[j] * i + value)/(i + 1)
     np_array = numpy.array(node_vector)
-    node = Node(np_array,i, None)
+    node = Node(np_array,i, None, matrix_wrapper )
     available_nodes.append(node)
   wrapped_mean_vector = numpy.array(mean_vector)
-  return available_nodes, unfinished_nodes, wrapped_mean_vector
+  return available_nodes, unfinished_nodes, wrapped_mean_vector,matrix_wrapper
 
-def process_match(match, available_nodes, unfinished_nodes, mean_vector):
+@profile
+def process_match(match, available_nodes, unfinished_nodes, mean_vector, matrix):
    node1, node2 = match.get_nodes()
-   node1_id, _, node1_vector = node1.get_info()
-   node2_id, _, node2_vector = node2.get_info()
+   node1_id, _, node1_vector,_ = node1.get_info()
+   node2_id, _, node2_vector, _ = node2.get_info()
    new_vector = (node1_vector + node2_vector) / 2
-   node1 = Node(new_vector, node1_id, node2_id)
-   node2 = Node(new_vector, node2_id, node1_id)
+   node1 = Node(new_vector, node1_id, node2_id, matrix)
+   node2 = Node(new_vector, node2_id, node1_id, matrix)
    if is_close(new_vector, mean_vector):
 	unfinished_nodes.pop(node1_id, None)
 	unfinished_nodes.pop(node2_id, None)
@@ -56,29 +71,32 @@ def process_match(match, available_nodes, unfinished_nodes, mean_vector):
    available_nodes.append(node1)  
    available_nodes.append(node2)  
 
+@profile
 def match_nodes(available_nodes, event_processing, current_time, limit):
     if len(available_nodes) < limit:
        return
-    i = 0
-    while i < len(available_nodes) - 1:
-       current_node = available_nodes[i]
-       _,_, vector = current_node.get_info()
-       max_index = i + 1
-       max_difference = -1
-       for j in range(i+1, len(available_nodes)):
+    while len(available_nodes) > 1:
+       current_node = available_nodes[0]
+       _,_,_, vector = current_node.get_info()
+       num_rows = len(available_nodes) - 1
+       num_cols = len(vector)
+       matrix = numpy.zeros([num_rows, num_cols], float)
+       for j in range(1, len(available_nodes)):
           temp_node = available_nodes[j]
-          _, _,temp_node_vector = temp_node.get_info()
-	  diff = numpy.linalg.norm(vector-temp_node_vector, 2)
-	  if diff > max_difference:
-             max_difference = diff
-	     max_index = j  
-       temp_node = available_nodes.pop(max_index)
-       available_nodes.pop(i)
+          _, _,_,temp_node_vector = temp_node.get_info()
+	  helper = vector-temp_node_vector
+	  matrix[j-1,:] = helper
+       result = numpy.linalg.norm(matrix, axis=1)
+       max_index = numpy.argmax(result)
+       temp_node = available_nodes.pop(max_index + 1)
+       available_nodes.pop(0)
        random_delay = get_exponential_delay() + current_time
        new_match = Match(current_node, temp_node)
        heappush(event_processing, (random_delay,new_match))        
+
+@profile
 def run_test(num_nodes):
-    available_nodes, unfinished_nodes, mean = setup(num_nodes)
+    available_nodes, unfinished_nodes, mean, matrix = setup(num_nodes)
     current_time = 0.0
     event_processing = []
     limit = math.floor(num_nodes * 0.1)
@@ -86,7 +104,7 @@ def run_test(num_nodes):
        match_nodes(available_nodes, event_processing, current_time, limit)
        new_time, match = heappop(event_processing)     
        current_time = new_time
-       process_match(match,  available_nodes, unfinished_nodes, mean)
+       process_match(match,  available_nodes, unfinished_nodes, mean, matrix)
     return current_time
 
 def run_multiple_tests(num_nodes):
@@ -95,19 +113,18 @@ def run_multiple_tests(num_nodes):
     for i in range(num_samples):
        time = run_test(num_nodes)
        average_time = (average_time * i + time)/(i+1)
-    print average_time
     return average_time
 
 def run():
    
-   num_nodes_list = [3, 7, 15, 31, 63,127,255,511,1023,2047,4095,8191]
+   anum_nodes_list = [3, 7, 15, 31, 63,127,255,511,1023,2047,4095,8191]
+   num_nodes_list = [3,7,15]
    times_list = []
    for num_nodes in num_nodes_list:
       average_time = run_multiple_tests(num_nodes) 
       times_list.append(average_time) 
-   print times_list
    plt.plot(num_nodes_list, times_list)
    plt.ylabel('time')
    plt.xlabel('num nodes')
    plt.show()
-run()
+run_test(8191)
--- gossip/node.py
+++ gossip/node.py
@@ -1,11 +1,21 @@
+import numpy
 class Node(object):
-  def __init__(self, vector,my_id, last_matched_id):
+  def __init__(self, vector,  my_id, last_matched_id, matrix):
       self.vector = vector
+      temp = []
+      chunk = 10
+      length = len(vector) / chunk
+      ##for i in range(length):
+          ##result = numpy.mean(vector[i*chunk : (i + 1) * chunk])
+          ##temp.append(result)
+      self.compressed_vector = numpy.dot(vector, matrix)
       self.my_id = my_id
       self.last_matched_id = last_matched_id
-  
+      if my_id == last_matched_id:
+	print "fuck fuck fuck", my_id
+        
   def get_info(self):
-      return self.my_id, self.last_matched_id, self.vector
+      return self.my_id, self.last_matched_id, self.vector, self.compressed_vector
 
   def __repr__(self):
         return "<my id %s target id:%s>" % (self.my_id, self.last_matched_id)
